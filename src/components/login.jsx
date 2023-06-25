67906217fdc2515b1953dddb39f2f497913e2e27
usmc1371nerd <james.p.dumas@gmail.com>
2023-06-25 09:20:45 -0500

    setup the auth/login, it now routes to search page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,19 +1,90 @@
-import React from 'react'
-import './login.css'
-import Header from './header'
-
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './login.css';
 
 const Login = () => {
-  return (
-    <div>
-      <Header/>
-      <div className="login-input">
-      <input type="text" placeholder='name'></input>
-      <input type="text" placeholder='email'/>
-      <button type="submit">Log-in</button>
-      </div>
-    </div>
-  )
-}
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const navigate = useNavigate();
 
-export default Login
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      // Send login request to the server
+      const response = await fetch('https://frontend-take-home-service.fetch.com/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, email }),
+        credentials: 'include', // Include cookies with the request
+      });
+
+      if (response.ok) {
+      
+        
+         navigate('/search'); // Redirect to the search page
+      } else {
+        
+        console.log('login failed')
+        // Login failed
+        // Handle error or display error message
+      }
+    } catch (error) {
+      // Handle network error
+    }
+  };
+
+  const handleGetCookie = async () => {
+    try {
+      const response = await fetch('https://frontend-take-home-service.fetch.com', {
+        method: 'GET',
+        credentials: 'include', // Include cookies with the request
+      });
+
+      if (response.ok) {
+        const cookies = response.headers.get('set-cookie');
+        if (cookies) {
+          const fetchAccessTokenCookie = cookies
+            .split(';')
+            .find((cookie) => cookie.startsWith('fetch-access-token'));
+
+          if (fetchAccessTokenCookie) {
+            const [, cookieValue] = fetchAccessTokenCookie.split('=');
+            // Set the cookie value to "Cookie" header for subsequent requests
+            document.cookie = `fetch-access-token=${cookieValue}`;
+          }
+        }
+      } else {
+        // Handle error or display error message
+      }
+    } catch (error) {
+      // Handle network error
+    }
+  };
+
+  useEffect(() => {
+    handleGetCookie();
+  }, []);
+
+  return (
+    <form onSubmit={handleLogin}>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
